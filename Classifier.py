612cd62e730d63de2bb58e51081f0587bae188e1
Ankitha <ankicool20@gmail.com>
2017-09-21 02:03:30 -0400

    Fixes for unknown, and perplexity code

--- Classifier.py
+++ Classifier.py
@@ -55,7 +55,6 @@ def compute_bigram_probability(tokens, unigram_frequency):
             else:
                 bigram_frequency[bigram] = 1
 
-    #bigram_count = len(bigram_frequency)
     bigram_probability = dict(bigram_frequency)
     for ((x, y), value) in bigram_probability.items():
         bigram_probability[(x, y)] = value / float(unigram_frequency[y])
@@ -71,24 +70,18 @@ def nested_bigram(bigrams, start_word):
     return x_bigrams
 
 
-def handle_unknowns_for_unigrams(unigram_frequency):
-    average_freq_in_unigram = sum(unigram_frequency.values())/len(unigram_frequency)
-    unigram_frequency["UNK"] = int(average_freq_in_unigram)
-    """keys = [key for key, value in unigram_frequency.items() if value == int(average_freq_in_unigram)]
-    if keys is not None:
-        unigram_frequency.pop(random.choice(keys), None)
-        unigram_frequency["UNK"] = int(average_freq_in_unigram)
-    else:
-        print("fix this soon")"""
-
-    return unigram_frequency
+def handle_unknowns_for_unigrams(unigram_frequency,tokens):
+    tokens = ["UNK" if token == "the" else token for token in tokens]
+    unigram_frequency["UNK"] = unigram_frequency.pop("the")
+    #print(unigram_frequency["UNK"])
+    return unigram_frequency, tokens
 
 
 def get_GoodTuring_counts(unigram_frequency, tokens):
     total_unigrams = len(unigram_frequency)
     total_bigrams = total_unigrams * total_unigrams
     bigrams_prob, bigrams_frequency = compute_bigram_probability(tokens, unigram_frequency)
-    bigrams_unseen_count = total_bigrams - sum(bigrams_frequency.values())
+    bigrams_unseen_count = total_bigrams - len(bigrams_frequency)
     print("bigrams_unseen_count " + str(bigrams_unseen_count))
 
     bigrams_count_list = [bigrams_unseen_count, 0, 0, 0, 0, 0]
@@ -99,6 +92,7 @@ def get_GoodTuring_counts(unigram_frequency, tokens):
     goodTuring_counts = [0, 0, 0, 0, 0]
     for x in range(0, 5):
         goodTuring_counts[x] = (x+1) * (bigrams_count_list[x+1] * 1.0000) / (bigrams_count_list[x] * 1.0000)
+    print(goodTuring_counts)
     return goodTuring_counts
 
 
@@ -110,82 +104,50 @@ def get_Good_Turing_probability(w1, w2, unigram_frequency, tokens, GT_counts):
     if adj_count < 5:
         adj_count = GT_counts[adj_count]
     if w1 in unigram_frequency.keys():
-        t = adj_count / (unigram_frequency[w1] * 1.0)
-        print(t)
-    else:
-        t = adj_count / (unigram_frequency["UNK"] * 1.0)
-        print(t)
-    return t
-    #get_Good_Turing_probability("Ankita", "Nayan", unigram_frequency_pos_unk,tokens_pos,GT_counts)
+        return adj_count / (unigram_frequency[w1] * 1.0)
 
 
-def calculate_perplexity(test_token, unigrams_count, tokens, GT_count):
+def calculate_perplexity(test_token, unigrams_frequency, tokens, GT_count):
     import math
-    #print(unigrams_count[test_token[0]])
     total_tokens = len(tokens)
     len_token = len(test_token)
     for i in range(0, len_token, 1):
-        if test_token[i] not in unigrams_count:
+        if test_token[i] not in unigrams_frequency:
             test_token[i] = u'UNK'
 
-        sum = math.log(unigrams_count[test_token[0]] * 1.0 / total_tokens) * (-1.0)
-        for x in range(0, len_token - 1):
-            prob = get_Good_Turing_probability(test_token[x], test_token[x + 1], unigrams_count, tokens, GT_count)
-            sum += ((math.log(prob)) * (-1))
-
-        return math.exp((sum / len_token))
-
-def sentiment_classifier():
-    pass
+    test_sum = math.log(unigrams_frequency[test_token[0]] * 1.0 / total_tokens) * (-1.0)
+    for x in range(0, len_token - 1):
+        prob = get_Good_Turing_probability(test_token[x], test_token[x+1], unigrams_frequency, tokens, GT_count)
+        test_sum += ((math.log(prob)) * (-1))
+    return math.exp((test_sum / len_token))
 
 
 def main():
     file_path_training_pos = "D:\PythonProjects\SentimentAnalysis\input\Train\pos.txt"
     file_path_training_neg = "D:\PythonProjects\SentimentAnalysis\input\Train\\neg.txt"
-    tokens_pos = default_tokenizer(open(file_path_training_pos,"r").read())
-    tokens_neg = default_tokenizer(open(file_path_training_neg,"r").read())
+    tokens_pos = default_tokenizer(open(file_path_training_pos, "r").read())
+    tokens_neg = default_tokenizer(open(file_path_training_neg, "r").read())
 
     unigram_frequency_pos = compute_unigram_frequency(tokens_pos)
     unigram_frequency_neg = compute_unigram_frequency(tokens_neg)
 
     file_path_dev_pos = "D:\PythonProjects\SentimentAnalysis\input\Dev\pos.txt"
     file_path_dev_neg = "D:\PythonProjects\SentimentAnalysis\input\Dev\\neg.txt"
-    tokens_dev_pos = default_tokenizer(open(file_path_dev_pos,"r").readline())
-    tokens_dev_neg = default_tokenizer(open(file_path_dev_neg,"r").readline())
+    tokens_dev_pos = default_tokenizer(open(file_path_dev_pos, "r").read())
+    tokens_dev_neg = default_tokenizer(open(file_path_dev_neg, "r").read())
 
-    #unigram_frequency_pos_dev = compute_unigram_frequency(tokens_dev_pos)
-    #unigram_frequency_neg_dev = compute_unigram_frequency(tokens_dev_neg)
-
-    positive_token_count = sum(unigram_frequency_pos.values())
-    negative_token_count = sum(unigram_frequency_neg.values())
-
-    print("positive_token_count " + str(positive_token_count))
-    print("negative_token_count " + str(negative_token_count))
-
-    unigram_freq_with_unk_pos = handle_unknowns_for_unigrams(unigram_frequency_pos)
-    unigram_probabilities_pos = compute_unigram_probability(unigram_freq_with_unk_pos)
-
-    unigram_freq_with_unk_neg = handle_unknowns_for_unigrams(unigram_frequency_neg)
-    unigram_probabilities_neg = compute_unigram_probability(unigram_freq_with_unk_neg)
-
-    print("prob of unknowns in positive " + str(unigram_probabilities_pos["UNK"]))
-    print("prob of unknowns in negative " + str(unigram_probabilities_neg["UNK"]))
+    unigram_freq_with_unk_pos, tokens_pos = handle_unknowns_for_unigrams(unigram_frequency_pos, tokens_pos)
+    unigram_freq_with_unk_neg, tokens_neg = handle_unknowns_for_unigrams(unigram_frequency_neg, tokens_neg)
 
     GT_counts_pos = get_GoodTuring_counts(unigram_freq_with_unk_pos, tokens_pos)
-    get_Good_Turing_probability("a", "movie", unigram_freq_with_unk_pos,tokens_pos,GT_counts_pos)
+    GT_counts_neg = get_GoodTuring_counts(unigram_freq_with_unk_neg, tokens_neg)
 
-    #test_bigram = ["most","part"]
+    positive_perplexity = calculate_perplexity(tokens_dev_pos, unigram_freq_with_unk_pos, tokens_pos, GT_counts_pos)
+    print("Perplexity for postive dev corpus" + str(positive_perplexity))
+    negative_perplexity = calculate_perplexity(tokens_dev_neg, unigram_freq_with_unk_neg, tokens_neg, GT_counts_neg)
+    print("Perplexity for negative dev corpus" + str(negative_perplexity))
 
-
-    #sentiment_classifier(unigram_freq_with_unk_pos,tokens_dev_pos)
-
-    #perplexity_score = calculate_perplexity(test_bigram, unigram_frequency_pos_unk, tokens_pos, GT_counts)
-    #print(perplexity_score)
 
 
 if __name__ == "__main__":
         main()
-
-
-
-
